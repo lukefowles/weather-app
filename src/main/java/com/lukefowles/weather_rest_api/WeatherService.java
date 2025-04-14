b879c30430e06c3d25014ab61b64c81c993c86fb
lukefowles <luke.fowles15@gmail.com>
2025-04-14 16:20:12 +1000

    added get mapping to controller class with test

---
+++ src/main/java/com/lukefowles/weather_rest_api/WeatherService.java
@@ -0,0 +1,11 @@
+package com.lukefowles.weather_rest_api;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    WeatherResponse getWeatherResponse(String city, String country) {
+        return null;
+    }
+}
