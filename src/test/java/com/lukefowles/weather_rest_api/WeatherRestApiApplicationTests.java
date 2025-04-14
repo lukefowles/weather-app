3517c0d142d19152fd25fb8e154a766b4dd0c7bb
lukefowles <luke.fowles15@gmail.com>
2025-04-14 14:13:33 +1000

    initial commit

---
+++ src/test/java/com/lukefowles/weather_rest_api/WeatherRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukefowles.weather_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
