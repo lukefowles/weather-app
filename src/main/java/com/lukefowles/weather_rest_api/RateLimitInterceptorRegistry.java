ddbe3a1ab858374399f389bff12ea2638ddd789d
lukefowles <luke.fowles15@gmail.com>
2025-04-14 18:04:57 +1000

    added rate limit to api keys

---
+++ src/main/java/com/lukefowles/weather_rest_api/RateLimitInterceptorRegistry.java
@@ -0,0 +1,21 @@
+package com.lukefowles.weather_rest_api;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RateLimitInterceptorRegistry implements WebMvcConfigurer {
+
+    private RateLimiter interceptor;
+
+    public RateLimitInterceptorRegistry(RateLimiter interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/weather");
+    }
+}
---
+++ src/main/java/com/lukefowles/weather_rest_api/RateLimiter.java
@@ -0,0 +1,58 @@
+package com.lukefowles.weather_rest_api;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.ConsumptionProbe;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class RateLimiter implements HandlerInterceptor {
+    private final Map<String, Bucket> cache = new ConcurrentHashMap<>();
+
+    public Bucket resolveBucket(String apiKey) {
+        return cache.computeIfAbsent(apiKey, this::newBucket);
+    }
+
+    private Bucket newBucket(String apiKey) {
+        return Bucket.builder()
+                .addLimit(newBandwidth(5, 1))
+                .build();
+    }
+
+    private Bandwidth newBandwidth(int tokenCount, int hourDuration) {
+        return Bandwidth.builder()
+                .capacity(tokenCount)
+                .refillGreedy(tokenCount, Duration.ofHours(hourDuration))
+                .build();
+    }
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        String apiKey = request.getHeader("X-API-KEY");
+        if (apiKey == null || apiKey.isEmpty()) {
+            response.sendError(HttpStatus.BAD_REQUEST.value(), "Missing Header: X-api-key");
+            return false;
+        }
+
+        Bucket tokenBucket = resolveBucket(apiKey);
+        ConsumptionProbe probe = tokenBucket.tryConsumeAndReturnRemaining(1);
+        if (probe.isConsumed()) {
+            response.addHeader("X-Rate-Limit-Remaining", String.valueOf(probe.getRemainingTokens()));
+            return true;
+        } else {
+            long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
+            response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill));
+            response.sendError(HttpStatus.TOO_MANY_REQUESTS.value(),
+                    "API rate limit exceeded");
+            return false;
+        }
+    }
+}
