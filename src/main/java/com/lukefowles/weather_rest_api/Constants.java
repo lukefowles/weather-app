440d0e5f6b358955e50a765b2c61e91a0326b2aa
lukefowles <luke.fowles15@gmail.com>
2025-04-15 15:48:21 +1000

    added build pipeline

--- src/main/java/com/lukefowles/weather_rest_api/Constants.java
+++ src/main/java/com/lukefowles/weather_rest_api/Constants.java
@@ -1,7 +1,7 @@
 package com.lukefowles.weather_rest_api;
 
 class Constants {
-    static final String API_SERVER_EXCEPTION = "Server exception for the API";
-    static final String LOCATION_NOT_FOUND = "Location data not found location passed";
+    static final String API_SERVER_EXCEPTION = "Server Error";
+    static final String LOCATION_NOT_FOUND = "Location Data Not Found";
     static final String BAD_REQUEST_EXCEPTION = "Parameters in the request are missing or incorrectly formatted";
 }
